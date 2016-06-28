9fc8799f5566550255c4355fd6773374cc6e3378
mahui <hui.ma@capitalonline.net>
2016-06-28 17:15:20 +0800

    修改libs目录位置，编辑main.js文件

--- WebApp/scripts/main.js
+++ WebApp/scripts/main.js
@@ -1,6 +1,13 @@
-requires.config({
+require.config({
     baseUrl:"scripts",
     paths:{
-        
-    }
+        "angular":"libs/angular1.4.12/angular",
+        "angular-messages":"libs/angular1.4.12/angular-messages.min",
+        "angular-cookies":"libs/angular1.4.12/angular-cookies.min",
+        "angular-route":"libs/angular1.4.12/angular-route.min",
+        "angular-zh-cn":"libs/angular1.4.12/angular-locale_zh-cn",
+        "jquery":"libs/jquery1.11.3/jquery-1.11.3.min",
+        "bootstrap":"libs/bootstrap-3.3.5/js/bootstrap.min"
+    },
+    urlArgs: "bust=" +  (new Date()).getTime()
 });
